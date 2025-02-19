ea21e04a188ab3fd2db44a82f20ea2d16a6d56a2
“Cosimo <pastore_cosimo@yahoo.it>
2025-02-19 16:59:37 +0100

    first commit

---
+++ domande.ts
@@ -0,0 +1,81 @@
+//1) Quali sono i tipi primitivi principali in TypeScript?
+console.log("1. I dati di tipo primitivo sono String, Number, Boolean, Undefined, null");
+
+//2) Crea tre variabili tipizzate: una stringa con il tuo nome, un numero con la tua età, e un booleano che indica se stai studiando TypeScript.
+console.log("2. let nome: string = 'Cosimo'; let età: number = 37; let studioTypeScript: boolean = true");
+
+//3) Tipizza il parametro della seguente funzione:
+
+var greet = (name: string) => {
+  return "Ciao " + name;
+};
+console.log(greet);
+
+//4) Specifica il tipo di ritorno della seguente funzione:
+
+var sum = (a: number, b: number): number => {
+  return a + b;
+};
+console.log(sum(3, 1));
+
+//5) Crea una funzione che accetti un prezzo e restituisca il prezzo con IVA (22%). Usa i tipi appropriati.
+
+var prezzoPiuIva = (prezzo: number): number => {
+  const iva = 0.22;
+  return prezzo + prezzo * iva;
+};
+console.log(prezzoPiuIva); //(100 + 100 * 0.22)
+
+//6) Crea una funzione che concateni due stringhe e restituisca la lunghezza totale.
+
+var concatenaStringhe = (str1: string, str2: string) => {
+  var concatenamento = str1 + str2;
+  return concatenamento.length;
+};
+console.log(concatenaStringhe("ciao", "mondo"));
+
+//7) Cos'è un Type Union e come si scrive?
+console.log("7. Il Type Union in TypeScript è una variabile che consente di unire diversi data type in uno solo ");
+
+var typeUnion = (a: number | string) => {
+  a = "ciao"; //ok
+  a = 18; //ok
+  //   a = true; //Non va bene perche non rientra nè in number nè in string. Risposta: Type 'boolean' is not assignable to type 'string | number'.
+};
+console.log(typeUnion);
+
+//8) Crea una variabile che possa contenere un numero, null o undefined.
+var mixVariable: number | boolean = 5;
+console.log(mixVariable);
+
+var mixVariable2: (string | undefined)[] = [];
+mixVariable2.push("1");
+mixVariable2.push(undefined);
+
+//9) Crea un tipo per rappresentare i giorni della settimana usando union di stringhe letterali.
+type giorniSettimana = "Lunedì" | "Martedì" | "Mercvoledì" | "Giovedì" | "Venerdì" | "Sabato" | "Domenica";
+
+//10) Tipizza il seguente array di numeri nei due modi possibili:
+
+//const numbers = [1, 2, 3];
+
+var method1: number[] = [1, 2, 3];
+console.log(method1);
+
+var method2: Array<number> = [];
+method2.push(1);
+method2.push(2);
+method2.push(3);
+console.log(method2);
+
+//11) Crea una tupla per definire un array di 5 elementi, i primi 3 devono essere stringhe e gli ultimi due numeri.
+
+var quellaTupla = ["string", "string", "string", "number", "number"];
+
+type quellaTupla = ["ciao", "mondo", "addio", 1, 2];
+console.log("quellaTupla");
+
+//12) Qual è la differenza tra type e interface?
+console.log(
+  "Entrambi usati per definire una tipologia. TYPE solitamente è usato per Union e Tuple (definendo tipi Primitivi), mentre le Interface possono essere ridefinite, sono molto simili al “type”, ma a differenza di quest’ultimo, ci permette di definire la forma di un oggetto, pittosto che un tipo di oggetto. "
+);
